56966983826d37a5cd416b050650cb9030229940
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-07 12:50:13 +0600

    all are completed

--- text.js
+++ text.js
@@ -19,3 +19,62 @@ function f2(event) {
         event.classList.add("active");
     }
 }
+function f3(event) {
+
+    if (textArea.style.fontStyle === "italic") {
+        textArea.style.fontStyle = "normal";
+        event.classList.remove("active");
+    } else {
+        textArea.style.fontStyle = "italic";
+        event.classList.add("active");
+    }
+}
+
+function f4(event) {
+
+    if (textArea.style.textDecoration == "underline") {
+        textArea.style.textDecoration = "none";
+        event.classList.remove("active");
+    } else {
+        textArea.style.textDecoration = "underline";
+        event.classList.add("active");
+    }
+}
+
+function f5(event) {
+    textArea.style.textAlign = "left";
+}
+function f6(event) {
+    textArea.style.textAlign = "center";
+}
+function f7(event) {
+    textArea.style.textAlign = "right";
+}
+
+function f8(event) {
+
+    if (textArea.style.textTransform == "uppercase") {
+        textArea.style.textTransform = "none";
+        event.classList.remove("active");
+    } else {
+        textArea.style.textTransform = "uppercase";
+        event.classList.add("active");
+    }
+}
+
+function f9() {
+    textArea.style.fontWeight = "normal";
+    textArea.style.fontStyle = "normal";
+    textArea.style.textAlign = "left";
+    textArea.style.textTransform = "capitalize";
+    textArea.value = "";
+}
+
+function f10(event) {
+    let value = event.value;
+    textArea.style.color = value;
+}
+
+window.addEventListener("load"), () => {
+    textArea.value = "";
+}
